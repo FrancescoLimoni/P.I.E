3b8f4afce8143284014b0f0dbe246ba44f5d40b3
nmille46 <47011917+nmille46@users.noreply.github.com>
2019-02-12 16:24:12 -0700

    Update Main.rb

--- pieProject/code/Main.rb
+++ pieProject/code/Main.rb
@@ -1,10 +1,13 @@
+$LOAD_PATH << '.'
+
 require 'fox16'
+require 'BrushPanel.rb'
 
 include Fox
 
 class EditorWindow < FXMainWindow
   def initialize(app)
-    super(app, "Pixel Image Editor", :width => 1080, :height => 720)
+    super(app, "Pixel Image Editor", :width => 500, :height => 500)
   end
   
   def create
@@ -16,7 +19,9 @@ end
 
 if __FILE__ == $0
   FXApp.new do |app|
-    EditorWindow.new(app)
+    editor_window = EditorWindow.new(app)
+    BrushPanel.new(editor_window,LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT,0,0,69,196)
+    FXToolTip.new(app)
     app.create
     app.run
   end
