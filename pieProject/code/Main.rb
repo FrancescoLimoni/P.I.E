18344e3006d04186c99b2c5dda5cf3443785998e
OmarBitar <obitar@yahoo.com>
2019-02-21 10:25:27 -0700

    adding floating ui class

--- pieProject/code/Main.rb
+++ pieProject/code/Main.rb
@@ -6,6 +6,7 @@ require 'BrushPanel.rb'
 require 'Menu'
 require 'fox16'
 require 'ColorPanel.rb'
+require 'FloatingToolBar.rb'
 
 include Fox
 
@@ -25,8 +26,12 @@ end
 
 if __FILE__ == $0
   FXApp.new do |app|
+
     editor_window = EditorWindow.new(app)
-    brush_window = BrushPanel.new(editor_window,LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT,0,0,69,196)
+    #adding floating ui to this Window
+    tool_bar = FloatingToolBar.new(editor_window,LAYOUT_SIDE_LEFT,0,0,0)
+
+    brush_window = BrushPanel.new(tool_bar.getToolBar,LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT,0,0,69,196)
     draw = Canvas.new(editor_window, app, FRAME_THICK| LAYOUT_CENTER_X || LAYOUT_CENTER_Y, 1000, 200, 250, 250, 0, 0, 0, 0)
     color_window = ColorPanel.new(editor_window, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_CENTER_X , 0, 0, 176, 55)
     FXToolTip.new(app)
