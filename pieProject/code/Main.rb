6d7c624a57eecffd595f90ad868256d9e5d89c23
OmarBitar <obitar@yahoo.com>
2019-02-26 16:07:46 -0700

    all panels can float

--- pieProject/code/Main.rb
+++ pieProject/code/Main.rb
@@ -42,6 +42,9 @@ if __FILE__ == $0
     #adding floating ui to this brush_tool_bar
     brush_tool_bar = FloatingToolBar.new(editor_window,LAYOUT_SIDE_LEFT,0,0,0)
 
+    #adding floating ui to this brush_tool_bar
+    layer_tool_bar = FloatingToolBar.new(editor_window,LAYOUT_SIDE_RIGHT,0,0,0)
+
     #adding floating ui to color_window
     color_tool_bar = FloatingToolBar.new(editor_window,LAYOUT_SIDE_BOTTOM,LAYOUT_CENTER_X,0,0)
     #adding empty dock for all toolbar objects
@@ -53,7 +56,7 @@ if __FILE__ == $0
     draw = Canvas.new(editor_window, app, FRAME_THICK| LAYOUT_CENTER_X || LAYOUT_CENTER_Y, 1000, 200, 250, 250, 0, 0, 0, 0)
     color_window = ColorPanel.new(color_tool_bar.getToolBar, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_CENTER_X , 0, 0, 176, 55)
 
-    layerPanel = LayerPanel.new(editor_window, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_SIDE_BOTTOM | LAYOUT_RIGHT, 0,0, 161, 120)
+    layerPanel = LayerPanel.new(layer_tool_bar.getToolBar, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_SIDE_BOTTOM | LAYOUT_RIGHT, 0,0, 161, 120)
 
     FXToolTip.new(app)
     app.create
