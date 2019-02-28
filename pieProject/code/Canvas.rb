e76fc5795f1f5c08cfd90e8a49cbbba99bc2d9ca
DustinShaver <36941002+DustinShaver@users.noreply.github.com>
2019-02-28 15:37:46 -0700

    Canvas Bug Fixes
    Fixes resize and redraw bugs.

--- pieProject/code/Canvas.rb
+++ pieProject/code/Canvas.rb
@@ -13,7 +13,7 @@ class Canvas
      padLeft, padRight, padTop, padBottom)
      
     @canvas_frame = FXVerticalFrame.new(@contents,FRAME_SUNKEN|LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT, 
-    0, 0, 0, 0, 10, 10, 10, 10) 
+   0, 0, 0, 0, 10, 10, 10, 10) 
     
     @canvas = FXCanvas.new(@canvas_frame, nil, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT, 0, 0, 0, 0,) 
   
@@ -22,9 +22,14 @@ class Canvas
     @drawColor = FXRGB(255, 0, 0)
     @mouseDown = false
     @dirty = false     
-  end
+    
+    @canvas.connect(SEL_PAINT) do |sender, sel, event|
+      FXDCWindow.new(@canvas,event) do |dc|
+        dc.foreground = @canvas.backColor
+        dc.fillRectangle(event.rect.x, event.rect.y, event.rect.w,  event.rect.h)
+      end
+    end
+   end
   self.instance_variables
-
 #self.connect(SEL_PAINT) do |sender, sel, event|
-  
 end
