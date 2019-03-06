6ecb7b1294f80757a1f7d8cdd4b7892c5e13bfd4
DustinShaver <36941002+DustinShaver@users.noreply.github.com>
2019-03-06 16:28:51 -0700

    Canvas Event Handlers
    Creates event handlers that are capable of:
    
    -Drawing a line,
    -Updating the canvas
    -detecting left-mouse press

--- Canvas.rb
+++ Canvas.rb
@@ -13,7 +13,7 @@ class Canvas
      padLeft, padRight, padTop, padBottom)
      
     @canvas_frame = FXVerticalFrame.new(@contents,FRAME_SUNKEN|LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT, 
-   0, 0, 0, 0, 10, 10, 10, 10) 
+   0, 0, 0, 0, 0, 0, 0, 0) 
     
     @canvas = FXCanvas.new(@canvas_frame, nil, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_TOP|LAYOUT_LEFT, 0, 0, 0, 0,) 
   
@@ -21,12 +21,64 @@ class Canvas
   
     @drawColor = FXRGB(255, 0, 0)
     @mouseDown = false
-    @dirty = false     
+    @dirty = false
+    @brushWidth = 10
+    @brushHeight = 1     
     
     @canvas.connect(SEL_PAINT) do |sender, sel, event|
       FXDCWindow.new(@canvas,event) do |dc|
         dc.foreground = @canvas.backColor
         dc.fillRectangle(event.rect.x, event.rect.y, event.rect.w,  event.rect.h)
+      end
+    end
+    @canvas.connect(SEL_LEFTBUTTONPRESS) do
+      @canvas.grab
+      @mouseDown = true
+      if @mousedown
+        dc = FXDCWindow.new(@canvas)
+        dc.foreground = @drawcolor
+        dc.drawLine(event.last_x, event.last_y, event.last_x + 1, event.last_y + 1)
+        @dirty = true
+        dc.end
+      end
+    end
+    @canvas.connect(SEL_MOTION) do |sender, sel, event|
+      if @mouseDown
+        # Get device context for the canvas
+        dc = FXDCWindow.new(@canvas)
+
+        # Set the foreground color for drawing
+        dc.foreground = @drawColor
+        dc.drawLine(event.last_x, event.last_y, event.win_x, event.win_y)
+
+        # We have drawn something, so now the canvas is dirty
+        @dirty = true
+
+        # Release the DC immediately
+        dc.end
+      end
+    end
+    
+    @canvas.connect(SEL_LEFTBUTTONRELEASE) do |sender, sel, event|
+      @canvas.ungrab
+      if @mouseDown
+        # Get device context for the canvas
+        dc = FXDCWindow.new(@canvas)
+
+        # Set the foreground color for drawing
+        dc.foreground = @drawColor
+
+        # Draw a line from the previous mouse coordinates to the current ones
+        dc.drawLine(event.last_x, event.last_y, event.win_x, event.win_y)
+
+        # We have drawn something, so now the canvas is dirty
+        @dirty = true
+
+        # Mouse no longer down
+        @mouseDown = false
+
+        # Release this DC immediately
+        dc.end
       end
     end
    end
