e71d21d53d9bffabdf1eaa90d25ed7792f70785c
Jacob Watters <jacobdwatters@gmail.com>
2019-04-10 14:39:45 -0600

    Added ability to directly change brush color via RGB values

--- pieProject/code/Main.rb
+++ pieProject/code/Main.rb
@@ -88,7 +88,7 @@ if __FILE__ == $0
     canvasPacker.backColor = "Gray69"
     splash_screen.addHideElement(canvasPacker,editor_window)
     
-    color_window = ColorPanel.new(color_tool_bar.getToolBar, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_CENTER_X , 0, 0, 150, 200, canvas_window)
+    color_window = ColorPanel.new(color_tool_bar.getToolBar, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_CENTER_X , 0, 0, 150, 280, canvas_window)
     color_window.backColor = "Gray69"
     splash_screen.addHideElement(color_window,editor_window)
 
