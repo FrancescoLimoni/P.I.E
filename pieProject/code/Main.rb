0211ac65564e9c76d7ead532dac05ebd3b633efd
Omar Bitar <obitar@yahoo.com>
2019-04-18 15:47:23 -0600

    updated main

--- pieProject/code/Main.rb
+++ pieProject/code/Main.rb
@@ -1,23 +1,103 @@
+$LOAD_PATH << '.'
+
+require 'Canvas.rb'
+require 'BrushPanel.rb'
+require 'Menu'
 require 'fox16'
+require 'ColorPanel.rb'
+require 'FloatingToolBar.rb'
+require 'layerPanel.rb'
+require 'SplashScreen.rb'
+require 'Color.rb'
+require 'launchy'
 
 include Fox
 
 class EditorWindow < FXMainWindow
-  def initialize(app)
-    super(app, "Pixel Image Editor", :width => 1080, :height => 720)
+  def initialize(app,logo,w,h)
+    super(app, "PIE", logo, logo, :width => w, :height => h)
+    #addMenuBar
+    floating_menu_bar = FloatingToolBar.new(self,LAYOUT_SIDE_TOP|LAYOUT_FILL_X)
+    @menuBar = MenuBar.new(app, floating_menu_bar.getToolBar, LAYOUT_SIDE_TOP , LAYOUT_FILL_X)
   end
   
   def create
     super
     show(PLACEMENT_SCREEN)
   end
+
+  #add friend funtion from splashscreen to menuBar
+  def editMenuBar(splashScreen)
+    puts 'creating new project'
+    @menuBar.splashScreenFriendfunction(splashScreen)
+  end
+  def canvaFriend(canvas)
+    @menuBar.canvasSaveFriendfunction(canvas)
+  end
 end
 
 
 if __FILE__ == $0
   FXApp.new do |app|
-    EditorWindow.new(app)
-    app.create
-    app.run
+
+    #load pie icon
+    icon = nil
+    File.open("icons/pie.png", "rb") do |io|
+      icon = FXPNGIcon.new(app, io.read)
+    end
+    icon
+
+    #call editor_window constructor
+    editor_window = EditorWindow.new(app,icon,400,400)
+    editor_window.backColor = "Gray20"
+    
+    #groupBoxV = FXGroupBox.new(packer, nil, :opts => FRAME_RIDGE | GROUPBOX_NORMAL)
+    #rightVFrame = FXHorizontalFrame.new(groupBoxV) 
+    
+    #create a splash screen object with editor_window as its scope
+    splash_screen = SplashScreen.new(editor_window, LAYOUT_CENTER_X | LAYOUT_CENTER_Y)
+    #add friend funtion from splashscreen to menuBar via editor_window
+    editor_window.editMenuBar(splash_screen)
+    
+    #adding floating ui to this brush_tool_bar
+    brush_tool_bar = FloatingToolBar.new(editor_window, LAYOUT_SIDE_LEFT)
+    splash_screen.addHideElement(brush_tool_bar.getToolBar,editor_window)
+
+    #make layer panel and color panel on top of each other
+    frameV = FXVerticalFrame.new(editor_window, :opts => LAYOUT_SIDE_RIGHT)
+    splash_screen.addHideElement(frameV,editor_window)
+
+    #adding floating ui to color_window
+    color_tool_bar = FloatingToolBar.new(frameV, LAYOUT_SIDE_RIGHT)
+    #adding empty dock for all toolbar objects
+    color_tool_bar.addDock(LAYOUT_SIDE_RIGHT)
+    splash_screen.addHideElement(color_tool_bar.getToolBar,editor_window)
+    
+    #adding floating ui to layerPanel
+    layer_tool_bar = FloatingToolBar.new(frameV,LAYOUT_SIDE_BOTTOM|LAYOUT_CENTER_X)
+    splash_screen.addHideElement(layer_tool_bar.getToolBar,editor_window)
+
+    canvasPacker = FXPacker.new(editor_window,LAYOUT_FILL_X|LAYOUT_FILL_Y)
+    canvas_window = Canvas.new(canvasPacker, LAYOUT_SIDE_TOP|LAYOUT_FILL_X|LAYOUT_FILL_Y, 0, 0, 800, 400, 0, 0, 0, 0, app)
+    canvasPacker.backColor = "Gray69"
+    splash_screen.addHideElement(canvasPacker,editor_window)
+    #add friend function to canvas and editor_window
+    editor_window.canvaFriend(canvas_window)
+    
+    brush_window = BrushPanel.new(brush_tool_bar.getToolBar,LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT,0,0,69,340, canvas_window)
+    brush_window.backColor = "Gray69"
+    splash_screen.addHideElement(brush_window,editor_window)
+    
+    color_window = ColorPanel.new(color_tool_bar.getToolBar, LAYOUT_FIX_WIDTH | LAYOUT_FIX_HEIGHT | LAYOUT_CENTER_X , 0, 0, 150, 280, canvas_window)
+    color_window.backColor = "Gray69"
+    splash_screen.addHideElement(color_window,editor_window)
+
+    layerPanel = LayerPanel.new(layer_tool_bar.getToolBar, LAYOUT_FIX_WIDTH | LAYOUT_FILL_Y | LAYOUT_SIDE_BOTTOM | LAYOUT_RIGHT, 0,0, 161, 120)
+    layerPanel.backColor = "Gray69"
+    splash_screen.addHideElement(layerPanel,editor_window)
+
+  FXToolTip.new(app)
+  app.create
+  app.run
   end
 end
--- pieProject/code/Menu.rb
+++ pieProject/code/Menu.rb
@@ -1,9 +1,123 @@
-require 'fox16'
+class MenuBar
+    def initialize(app,x,y,z)
+        puts "creating menu bar"
 
-include Fox
+        @splashScreenFriendObject = nil
+        @canvasSaveMethod = nil
+  
+        # set up menu LayerPanel properties ======================================
 
-class Menu < FXMenuBar
-  def initialize(app)
-    super
-  end
- end
+        menuBar = FXMenuBar.new(x, y | z)  
+        menuBar.backColor = "Gray69"
+            # create pointers (to link new 'FXMenuPane' wutg a new 'FXMenuTitle')
+        fileMenu = FXMenuPane.new(x)  #self refers to the TextEditor window
+        about = FXMenuPane.new(x)  #self refers to the TextEditor window
+
+        #load icons functions ==================================================
+        def readIcon(scope,path)
+            icon = nil
+            File.open(path, "rb") do |io|
+            icon = FXPNGIcon.new(scope, io.read)
+            icon.scale(25,25)
+            end
+            icon
+        end
+
+        #create main menu Items ================================================
+        
+        fileTab = FXMenuTitle.new(menuBar, nil, readIcon(app,"icons/pie.png"), :popupMenu => fileMenu)  
+        fileTab.backColor = "Gray69"
+
+        #btns
+        fileNew = FXButton.new(menuBar,"\tCreate New Canvas",:icon => readIcon(app,"icons/filenew.png"))
+        fileSave = FXButton.new(menuBar,"\tQuick Save",:icon => readIcon(app,"icons/filesave.png"))
+        fileSave_s = FXButton.new(menuBar,"\tSave new canvas",:icon => readIcon(app,"icons/filesaves.png"))
+        fileLoad = FXButton.new(menuBar,"\tLoad Canvas",:icon => readIcon(app,"icons/fileopen.png"))
+        aboutTab = FXButton.new(menuBar,"About")
+        
+        #create sub menue Items=================================================
+
+            #under 'File' tab 
+        newCmd = FXMenuCommand.new(fileMenu, "New")
+        loadCmd = FXMenuCommand.new(fileMenu, "Load")
+        saveCmd = FXMenuCommand.new(fileMenu, "Save") 
+        gitHubLink = FXMenuCommand.new(fileMenu, "GitHub")
+        exitCmd = FXMenuCommand.new(fileMenu, "Exit")
+
+            #under 'About' tab
+        aboutCmd = FXMenuCommand.new(about, "contact us") 
+        
+        #splash screen friend function=================================================
+        def splashScreenFriendfunction(splahsScreenObject)
+            @splashScreenFriendObject = splahsScreenObject
+        end
+        #splash screen friend function=================================================
+        def canvasSaveFriendfunction(canvasObject)
+            @canvasSaveMethod = canvasObject
+        end
+        #connect sub menue Items to functions =====================================
+        fileNew.connect(SEL_COMMAND) do
+            # 'forFriendfunctions' is defined in the splashScreen class
+            @splashScreenFriendObject.forFriendfunctions
+        end
+        #**************************************************************************
+        newCmd.connect(SEL_COMMAND) do
+            #@txt.text = ""
+            @splashScreenFriendObject.forFriendfunctions
+        end
+        #**************************************************************************
+        loadCmd.connect(SEL_COMMAND) do  
+            dialog = FXFileDialog.new(x, "Load a File") 
+            dialog.selectMode = SELECTFILE_EXISTING 
+            dialog.patternList = ["All Files (*)"]  
+            if dialog.execute != 0  
+            load_file(dialog.filename)  
+            end   
+        end
+        #**************************************************************************
+        fileLoad.connect(SEL_COMMAND) do  
+            dialog = FXFileDialog.new(x, "Load a File") 
+            dialog.selectMode = SELECTFILE_EXISTING 
+            dialog.patternList = ["All Files (*)"]  
+            if dialog.execute != 0  
+            load_file(dialog.filename)  
+            end   
+        end  
+        #**************************************************************************
+        saveCmd.connect(SEL_COMMAND) do
+            dialog = FXFileDialog.new(x, "Save a File")  
+            dialog.selectMode = SELECTFILE_EXISTING  
+            dialog.patternList = ["All Files (*)"]  
+            if dialog.execute != 0  
+                save_file(dialog.filename)  
+            end
+        end
+        #**************************************************************************
+        fileSave_s.connect(SEL_COMMAND) do
+            @canvasSaveMethod.save
+            puts ('long save')
+        end
+        #**************************************************************************
+        fileSave.connect(SEL_COMMAND) do
+            puts "quick save"
+        end
+        #**************************************************************************
+        exitCmd.connect(SEL_COMMAND) do
+            puts "exiting program"
+            exit
+        end
+        #**************************************************************************
+        aboutTab.connect(SEL_COMMAND) do
+            #Launchy.open("https://github.com/FrancescoLimoni/P.I.E")
+            puts 'opened browser'
+            Launchy.open(File.expand_path("../..", Dir.pwd)+"/PIEDOC.html")
+        end
+        #**************************************************************************
+        gitHubLink.connect(SEL_COMMAND) do
+            Launchy.open("https://github.com/FrancescoLimoni/P.I.E")
+            puts 'opening gitHub in browser'
+        end
+    
+    end 
+
+end
